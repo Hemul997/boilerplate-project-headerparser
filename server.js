ff4e5b3e05db7f1356d23b58d7458c479cf38d50
Hemul997 <nikitamoshkin1997@gmail.com>
2022-02-14 13:49:41 +0300

    Implement GET method for whoami request

--- server.js
+++ server.js
@@ -31,3 +31,18 @@ app.get("/api/hello", function (req, res) {
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+
+
+app.get("/api/whoami", function(req, res) {
+  typeof req.headers['x-forwarded-for'] == "undefined" ? ipaddress = req.ip 
+    : ipaddress = req.headers['x-forwarded-for'];
+
+  res.json({
+    "ipaddress": ipaddress
+    , "language": req.headers['accept-language']
+    , "software": req.headers['user-agent']
+  });
+  console.log(req.headers);
+
+});
